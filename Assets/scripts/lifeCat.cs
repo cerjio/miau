98a0a55c6da508ce0e88248daaa23ddf2cd9bafe
Valentin Granados <valefcfhc@gmail.com>
2015-01-24 17:16:58 -0600

    e

--- Assets/scripts/lifeCat.cs
+++ Assets/scripts/lifeCat.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class lifeCat : MonoBehaviour {
 
+	public int life;
+
 	// Use this for initialization
 	void Start () {
-	
+		life = 7;
 	}
 	
 	// Update is called once per frame
