bbb9f82f96c5b8f83640c3c910a75f29e714330f
cerjio <cerjio@gmail.com>
2015-01-24 07:45:33 -0600

    Se agregan cambios a escena 7

---
+++ Assets/scripts/MegamanControllerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MegamanControllerScript : MonoBehaviour {
+
+	public float maxspeed = 10f;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+	
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate() {
+		float move = Input.GetAxis ("Horizontal");
+		anim.SetFloat ("Speed", Mathf.Abs(move));
+		rigidbody2D.velocity = new Vector2 (move*maxspeed, rigidbody2D.velocity.y);
+	}
+}
