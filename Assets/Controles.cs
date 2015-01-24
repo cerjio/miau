777ffe60160cecc27dca091413fd93e428223c4a
sergio ivan lopez monzon <sergioivan154@gmail.com>
2015-01-24 10:34:38 -0600

    Controles

---
+++ Assets/Controles.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controles : MonoBehaviour {
+	public Texture btnTexture;
+	public GameObject player;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		speed = 5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI () {
+		
+		if (!btnTexture) {
+
+			Debug.LogError("Please assign a texture on the inspector");
+			return;
+		}
+		
+
+
+		while (GUI.RepeatButton(new Rect(5,Screen.height-(btnTexture.height/4),btnTexture.width/4,btnTexture.height/4), btnTexture) ){
+			player.transform.Translate(speed*Vector3.left*Time.deltaTime);
+			Debug.Log("izq");
+
+		}
+		
+		// Make the second button.
+		while (GUI.RepeatButton(new Rect((btnTexture.width/4)+5,Screen.height-(btnTexture.height/4),btnTexture.width/4,btnTexture.height/4),btnTexture ) ){
+			player.transform.Translate(speed*Vector3.right*Time.deltaTime);
+			Debug.Log("der");
+			
+		}
+	}
+}
