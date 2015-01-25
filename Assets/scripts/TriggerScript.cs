98ea0328e28c30ff923d6246465f5c6a6c15e159
cerjio <cerjio@gmail.com>
2015-01-24 20:11:39 -0600

    Subiendo el script

---
+++ Assets/scripts/TriggerScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerScript : MonoBehaviour {
+
+	/*
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}*/
+
+	void OnCollisionEnter2D(Collision2D coll) {
+
+		if (coll.gameObject.tag == "gato5") {
+
+			GameObject honeycomb = GameObject.FindGameObjectWithTag ("honeycomb");
+			honeycomb.rigidbody2D.isKinematic = false;
+		}
+
+
+	}
+}
