a2a96d65dbefb9eeda4a68314d06fd0bd72b6b59
Valentin Granados <valefcfhc@gmail.com>
2015-01-25 03:40:06 -0600

    fondo

--- Assets/scripts/MegamanControllerScript.cs
+++ Assets/scripts/MegamanControllerScript.cs
@@ -7,6 +7,8 @@ public class MegamanControllerScript : MonoBehaviour {
 	Animator anim;
 	public Texture btnTexture;
 
+	public SpriteRenderer fondo2, fondo3, fondo4;
+
 	bool faceRight = true;
 	public float Respawntime=1.0f;
 	public float acel=1;
@@ -23,11 +25,11 @@ public class MegamanControllerScript : MonoBehaviour {
 	void Start () {
 		//se llama al animator del personaje
 		anim = GetComponent<Animator> ();
-		//respawnPosition.position = GameObject.FindGameObjectWithTag ("Spawn").transform.position;
 
 		//Controles
 		player = GameObject.FindGameObjectWithTag ("Player");
 		speed = 5f;
+
 	}
 	
 	void FixedUpdate() {
@@ -80,7 +82,12 @@ public class MegamanControllerScript : MonoBehaviour {
 		
 		
 		while (GUI.RepeatButton(new Rect(5,Screen.height-(btnTexture.height/4),btnTexture.width/4,btnTexture.height/4), btnTexture) ){
-			player.transform.Translate(new Vector3(-(0.3f),0,0));
+			player.transform.Translate(new Vector3(-(0.05f),0,0));
+
+			fondo2.transform.Translate(new Vector3((0.01f),0,0));
+			fondo3.transform.Translate(new Vector3((0.02f),0,0));
+			fondo4.transform.Translate(new Vector3((0.04f),0,0));
+
 			Debug.Log("izq");
 			if(faceRight)
 			{
@@ -92,7 +99,12 @@ public class MegamanControllerScript : MonoBehaviour {
 		
 		// Make the second button.
 		while (GUI.RepeatButton(new Rect((btnTexture.width/4)+5,Screen.height-(btnTexture.height/4),btnTexture.width/4,btnTexture.height/4),btnTexture ) ){
-			player.transform.Translate(new Vector3((0.3f),0,0));
+			player.transform.Translate(new Vector3((0.05f),0,0));
+
+			fondo2.transform.Translate(new Vector3(-(0.01f),0,0));
+			fondo3.transform.Translate(new Vector3(-(0.02f),0,0));
+			fondo4.transform.Translate(new Vector3(-(0.04f),0,0));
+
 			Debug.Log("der");
 			if(!faceRight)
 			{
@@ -100,20 +112,63 @@ public class MegamanControllerScript : MonoBehaviour {
 				faceRight=true;
 			}
 		}
-		
-		// Make the third button.
-		while (GUI.RepeatButton(new Rect((btnTexture.width/4)+5,Screen.height-(btnTexture.height/4),btnTexture.width/4,btnTexture.height/4),btnTexture ) ){
-			player.transform.Translate(new Vector3((0.3f),0,0));
-			Debug.Log("der");
-			if(!faceRight)
-			{
-				Flip();
-				faceRight=true;
-			}
-		}
+
 	}
 	
 	
 	
 	
 }
+
+
+/*
+ * public float velocidadDespzamiento;
+	public float velocidadGiro;
+
+	public Transform fondo;
+
+	// Use this for initialization
+	void Start () {
+		try{
+			fondo = GameObject.FindGameObjectWithTag("fondo1").transform; 
+		}catch{
+
+		}
+	}
+
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "treeHole")
+						Destroy (coll.gameObject);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey(KeyCode.LeftArrow))	
+		{
+			this.transform.Translate(Vector3.left * velocidadDespzamiento);
+
+			try{
+			fondo.Translate(Vector3.left * velocidadDespzamiento);
+			}catch{}
+			//
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow))	
+		{
+			this.transform.Translate(Vector3.right * velocidadDespzamiento);	
+		}
+
+		if (Input.GetKey(KeyCode.UpArrow))	
+		{
+			this.transform.Translate(Vector3.up * velocidadDespzamiento);	
+		}
+
+		if (Input.GetKey(KeyCode.DownArrow))	
+		{
+			this.transform.Translate(Vector3.down * velocidadDespzamiento);	
+		}
+	}
+}
+*/
