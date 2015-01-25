3d1f91c128998972707077fb7db9873a3afcdd73
Valentin Granados <valefcfhc@gmail.com>
2015-01-24 18:43:44 -0600

    -

--- Assets/scripts/CambiarEscena.cs
+++ Assets/scripts/CambiarEscena.cs
@@ -11,31 +11,10 @@ public class CambiarEscena : MonoBehaviour {
 	// Update is called once per frame
 	
 	void Update(){
-		Debug.Log(Application.loadedLevelName);
+		Debug.Log(this.transform.position.x);
 
-		if(Application.loadedLevelName == "Escena1_perro" && this.transform.position.x >= 10){
-			Application.LoadLevel("Escena2_Atropellado");
+		if(this.transform.position.x >= 10){
+			Application.LoadLevel("Escena7_Panal");
 		}
-
-		else if(Application.loadedLevelName == "Escena2_Atropellado" && this.transform.position.x >= 10){
-			Application.LoadLevel("Escena3_piano");
-		}
-
-		else if(Application.loadedLevelName == "Escena3_piano" && this.transform.position.x >= 10){
-			Application.LoadLevel("Escena4_lata");
-		}
-
-		else if(Application.loadedLevelName == "Escena4_lata" && this.transform.position.x >= 10){
-			Application.LoadLevel("Escena5_panal");
-		}
-
-		else if(Application.loadedLevelName == "Escena5_panal" && this.transform.position.x >= 10){
-			Application.LoadLevel("Escena6_caminos");
-		}
-
-		else if(Application.loadedLevelName == "Escena6_caminos" && this.transform.position.x >= 10){
-			Application.LoadLevel("sceneFinish");
-		}
-
 	}
 }
