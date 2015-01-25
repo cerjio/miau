92f7e77afe5f08f9f8b9a7f1471156a5580ede10
Salvador Munguia <salva_bomba@hotmail.com>
2015-01-25 09:22:40 -0600

    Escena 2

---
+++ Assets/scripts/Controlgato.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controlgato : MonoBehaviour {
+	Vector3 scalar;
+	// Use this for initialization
+	void Start () {
+		scalar = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.UpArrow))
+		{
+			this.transform.Translate(Vector3.up * .01f);
+
+			scalar.x += -.0023f;
+			scalar.y += -.0023f;
+			this.transform.localScale=scalar;
+			if(this.transform.localScale.y<=.9 && this.transform.localScale.y>.6)
+			{
+				this.gameObject.renderer.sortingOrder=5;
+			}
+			if(this.transform.localScale.y<=.6 && this.transform.localScale.y>.46)
+			{
+				this.gameObject.renderer.sortingOrder=4;
+			}
+			if(this.transform.localScale.y<=.46 && this.transform.localScale.y>.35)
+			{
+				this.gameObject.renderer.sortingOrder=3;
+			}
+			if(this.transform.localScale.y<=.35)
+			{
+				this.gameObject.renderer.sortingOrder=2;
+			}
+
+		}
+		if(Input.GetKey(KeyCode.DownArrow))
+		{
+			this.transform.Translate(Vector3.down * .01f);
+			scalar.x += .0023f;
+			scalar.y += .0023f;
+			this.transform.localScale=scalar;
+		}
+	}
+}
--- Assets/scripts/MoverCarros.cs
+++ Assets/scripts/MoverCarros.cs
@@ -11,67 +11,54 @@ public class MoverCarros : MonoBehaviour {
 		posIni = 4;
 		tipoV = Random.Range (1, 6);
 		pasos = 0;
-
-	
+		
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		
 		switch (tipoV) 
 			
 		{
 		case 1:
-			velocidad=0.05f;
+			velocidad=0.07f;
 			break;
 		case 2:
-			velocidad=0.04f;
+			velocidad=0.06f;
 			break;
 		case 3:
-			velocidad=0.08f;
+			velocidad=0.1f;
 			break;
 		case 4:
-			velocidad=0.03f;
+			velocidad=0.11f;
 			break;
 		case 5:
-			velocidad=0.09f;
+			velocidad=0.13f;
 			break;
 		case 6:
-			velocidad=0.14f;
+			velocidad=0.18f;
 			break;
 			
 		}
-		if (gameObject.tag == "CarroArriba1") {
-	
-						if (this.transform.position.y <= -6) {
+		
+		
+		if (this.transform.position.x >=11) {
 			
-								this.transform.Translate (Vector3.down * velocidad * -pasos);
-								pasos = 0;
-								tipoV = Random.Range (1, 6);
-						} else {
-								this.transform.Translate (Vector3.down * velocidad);
-								pasos += 1;
-								Debug.Log (this.transform.position.y);
-						}
-		} if (gameObject.tag == "CarroAbajo") 
-		{
+			this.transform.Translate (Vector3.right * velocidad * -pasos);
+			pasos = 0;
+			tipoV = Random.Range (1, 6);
+		} else {
+			this.transform.Translate (Vector3.right * velocidad);
+			pasos += 1;
 			
-			if (this.transform.position.y >= 6) {
-				
-				this.transform.Translate (Vector3.down * velocidad * pasos);
-				pasos = 0;
-				tipoV = Random.Range (1, 6);
-			} else {
-				this.transform.Translate (Vector3.up * velocidad);
-				pasos += 1;
-				Debug.Log (this.transform.position.y);
-			}
-				}
+		}
+		
 		
 	}
-
+	
 	void OnCollisionEnter2D(Collision2D col)
-
+		
 	{
 		Destroy (col.gameObject);
 	}
