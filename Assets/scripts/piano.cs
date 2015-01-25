a1e4ffaeec1a6f92c6b320618c358485058e78de
tiranox85 <tiranox85@hotmail.com>
2015-01-24 19:58:15 -0600

    pianos

---
+++ Assets/scripts/piano.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class piano : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter2D(Collision2D coll) {
+		
+		//if(this.rigidbody.velocity.magnitude >0.1){
+		Destroy (this.gameObject);
+		
+		
+		
+		
+	}
+}
